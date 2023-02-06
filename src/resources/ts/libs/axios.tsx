4988dcfe738cc58f317486ea7c04e0958bca1ad8
h.kawai <hironori86@gmail.com>
2023-02-06 22:43:21 +0900

    production用デバッグコード追加

--- src/resources/ts/libs/axios.tsx
+++ src/resources/ts/libs/axios.tsx
@@ -2,6 +2,8 @@ import Axios from "axios";
 
 const isProd = (process.env.MIX_APP_ENV === 'production' ? true : false);
 
+console.log(process.env.MIX_APP_ENV, isProd);
+
 const axios = Axios.create({
     baseURL: isProd ? 'http://my-move.jp/' : "http://localhost:8080",
     headers: { "X-Requested-With": "XMLHttpRequest" },
